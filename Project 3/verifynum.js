b3a8d9c615bb08717a41f8575d8c1ed3769e0937
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-09-25 01:21:14 -0400

    still working

--- Project 3/verifynum.js
+++ Project 3/verifynum.js
@@ -1,12 +1,28 @@
-var inputField;
-var submitButton;
+var numverifyEndpoint = 'http://apilayer.net/api/validate';
+var numverifyKey = 'f7280f3f9e7e4bef446722c1923f2179';
+var googleMapsEndpoint = 'https://www.google.com/maps/embed/v1/place';
+var googleMapsKey = 'AIzaSyD4lO2enIyK50vGbUHfM969s6Z4m_OfBUo';
+
+var inputFieldElement
+var submitButtonElement;
+var numEnteredElement;
+var numValidityElement;
+var countryNameElement;
+var lineTypeElement;
+var countryMapElement;
 
 var onLoad = function() {
-  inputField = document.getElementById("inputField");
-  submitButton = document.getElementById("submitButton");
-  submitButton.onclick = verifyNumber.bind(submitButton, inputField.value)
-  inputField.addEventListener("keydown", function(event) {
-    if (event.keyCode < 48 || event.keyCode > 57) {
+  inputFieldElement = document.getElementById('inputField');
+  submitButtonElement = document.getElementById('submitButton');
+  numEnteredElement = document.getElementById('numEntered');
+  numValidityElement = document.getElementById('numValidity');
+  countryNameElement = document.getElementById('countryName');
+  lineTypeElement = document.getElementById('lineType');
+  countryMapElement = document.getElementById('countryMap');
+
+  submitButton.onclick = verifyNumber.bind(submitButtonElement, inputField.value)
+  inputField.addEventListener('keydown', function(event) {
+    if ((event.keyCode < 48 || event.keyCode > 57) && event.keyCode != 8) {
       event.preventDefault();
     }
     if (event.keyCode == 13) {
@@ -16,8 +32,51 @@ var onLoad = function() {
 }
 
 var verifyNumber = function() {
-  console.log("Submitted!\n")
+  var numEntered, numValidity, countryName, lineType, nonsuccessfulCall, errorMsg;
+
+  axios.get(numverifyEndpoint, {
+      params: {
+        access_key: numverifyKey,
+        number: inputField.value
+      }
+    })
+    .then(function (response) {
+      nonsuccessfulCall = (response['data']['success'] != null);
+      if (nonsuccessfulCall) {
+        errorMsg = response['data']['error']['info'];
+        numEnteredElement.innerHTML = "Error! " + errorMsg;
+      } else {
+        numEntered = response['data']['number'];
+        numValidity = response['data']['valid'];
+        countryName = response['data']['country_name'];
+        lineType = response['data']['line_type'];
+        numEnteredElement.innerHTML = "You requested: " + numEntered;
+
+        if (numValidity == true) {
+          numValidityElement.innerHTML = "Validity: VALID";
+        } else {
+          numValidityElement.innerHTML = "Validity: INVALID"
+        }
+
+        if (countryName.length != 0) {
+          countryNameElement.innerHTML = "Country Name: " + countryName.toUpperCase();
+          countryMapElement.src = googleMapsEndpoint + '?key=' + googleMapsKey + '&q=' + countryName;
+          countryMapElement.style.visibility = 'visible';
+        } else {
+          countryNameElement.innerHTML = "Country Name: UNAVAILABLE";
+        }
+
+        if (lineType != null) {
+          lineTypeElement.innerHTML = "Line Type: " + lineType.toUpperCase();
+        } else {
+          lineTypeElement.innerHTML = "Line Type: UNAVAILABLE";
+        }
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 /* initialize input field and submit button when page is loaded */
-window.addEventListener("load", onLoad);
+window.addEventListener('load', onLoad);
