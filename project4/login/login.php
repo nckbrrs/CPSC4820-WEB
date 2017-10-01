7abc07633bd5ce8582a660e57734f7a3aa89678c
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-10-01 17:54:53 -0400

    p4

--- project4/login/login.php
+++ project4/login/login.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require "UserStore.php";
 
 function validate() {
@@ -48,7 +47,7 @@ if(isset($_POST['comingBack'])){
 <title>Login</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<script type="text/javascript">var errorMessage = <?php /*php executes this blob of code */echo $errorMessage; ?>;</script>
+<script type="text/javascript">var errorMessage = "<?php /*php executes this blob of code */echo $errorMessage; ?>";</script>
 <style type="text/css">
   html {
     display: block;
