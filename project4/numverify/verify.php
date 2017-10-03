e9875dc03cbd8a181fb2396a82a33511dbe2a59b
Nick Barrs <nickbarrs@218.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-03 12:59:39 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -9,20 +9,5 @@ if (!isset($_SESSION['username'])) {
 $numverifyEndpoint = "http://apilayer.net/api/validate";
 $numverifyKey = "f7280f3f9e7e4bef446722c1923f2179";
 
-echo file_get_contents($numverifyEndpoint."?access_key=".$numverifyKey."&number=".$_GET['number']);
+return file_get_contents($numverifyEndpoint."?access_key=".$numverifyKey."&number=".$_GET['number']);
 ?>
-
-<!DOCTYPE html>
-<title>Verify</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<style type="text/css">
-  html {
-    display: block;
-    margin: auto;
-    text-align: center;
-    font-family: sans-serif;
-  }
-</style>
-<h1>You shouldn't be here</h1>
