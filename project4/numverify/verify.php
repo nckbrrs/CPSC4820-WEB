7476553e30ff6154cec516a5c8ddce7cb9ca7411
Nick Barrs <nickbarrs@222.180.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-02 23:04:41 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -9,9 +9,8 @@ $numverifyEndpoint = "http://apilayer.net/api/validate";
 $numverifyKey = "f7280f3f9e7e4bef446722c1923f2179";
 
 //$fgc = file_get_contents("http://apilayer.net/api/validate?access_key=f7280f3f9e7e4bef446722c1923f2179&number=".$_GET['number'])
-echo "hi there";
-return "hi there";
- ?>
+
+?>
 
 <!DOCTYPE html>
 <title>Hi</title>
