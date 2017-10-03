91046fd8ca05c6dbf5c6ae3e988eb09c416d25a2
Nick Barrs <nickbarrs@222.180.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-02 23:02:11 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['username'])) {
 $numverifyEndpoint = "http://apilayer.net/api/validate";
 $numverifyKey = "f7280f3f9e7e4bef446722c1923f2179";
 
-$fgc = file_get_contents("http://apilayer.net/api/validate?access_key=f7280f3f9e7e4bef446722c1923f2179&number=".$_GET['number'])
+//$fgc = file_get_contents("http://apilayer.net/api/validate?access_key=f7280f3f9e7e4bef446722c1923f2179&number=".$_GET['number'])
 echo "hi there";
 return "hi there";
  ?>
@@ -16,4 +16,3 @@ return "hi there";
  <!DOCTYPE html>
  <title>Hi</title>
  <h1>VERIFY</h1>
- 
