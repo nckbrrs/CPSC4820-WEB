d3403f25bf2d511ba5ae3680addfc70beb22e440
Nick Barrs <nickbarrs@222.180.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-02 23:06:54 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -5,12 +5,12 @@ session_start();
 if (!isset($_SESSION['username'])) {
   header("Location: /project4/login/index.html", true, 302);
 }
-/*
+
 $numverifyEndpoint = "http://apilayer.net/api/validate";
 $numverifyKey = "f7280f3f9e7e4bef446722c1923f2179";
 
 $fgc = file_get_contents("http://apilayer.net/api/validate?access_key=f7280f3f9e7e4bef446722c1923f2179&number=".$_GET['number'])
-*/
+
 ?>
 
 <!DOCTYPE html>
