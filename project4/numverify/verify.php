74932781aa58ea4db66ceda3de9ee2b41b66a976
Nick Barrs <nickbarrs@218.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-03 14:02:54 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -15,17 +15,3 @@ $numverifyKey = "f7280f3f9e7e4bef446722c1923f2179";
 echo file_get_contents($numverifyEndpoint."?access_key=".$numverifyKey."&number=".$_GET['number']);
 
 ?>
-
-<!DOCTYPE html>
-<title>Verify</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-<style type="text/css">
-  html {
-    display: block;
-    margin: auto;
-    text-align: center;
-    font-family: sans-serif;
-  }
-</style>
-<h1>Numverify Phone Number Verification</h1>
-<a href="/project4/numverify/phoneNumber.php">Click here to go to phone number verification page.</a>
