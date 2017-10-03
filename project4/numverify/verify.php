c685c117f19c12aacdaf28cc066a05548a9c18e3
Nick Barrs <nickbarrs@218.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-03 14:02:12 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -19,7 +19,6 @@ echo file_get_contents($numverifyEndpoint."?access_key=".$numverifyKey."&number=
 <!DOCTYPE html>
 <title>Verify</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <style type="text/css">
   html {
     display: block;
