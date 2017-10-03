ca37f0cc50e5d1171257480061671a2a859e45cb
Nick Barrs <nickbarrs@218.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-03 13:59:38 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -7,12 +7,6 @@ if (!isset($_SESSION['username'])) {
   header("Location: /project4/login/index.html", true, 302);
 }
 
-// otherwise, redirect to phoneNumber.php
-// since there is no reason for a user to need to access this page
-else {
-  header("Location: /project4/numverify/phoneNumber.php", true, 302);
-}
-
 // numverify variables
 $numverifyEndpoint = "http://apilayer.net/api/validate";
 $numverifyKey = "f7280f3f9e7e4bef446722c1923f2179";
@@ -22,3 +16,18 @@ $response = file_get_contents($numverifyEndpoint."?access_key=".$numverifyKey."&
 echo $response;
 
 ?>
+
+<!DOCTYPE html>
+<title>Verify</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<style type="text/css">
+  html {
+    display: block;
+    margin: auto;
+    text-align: center;
+    font-family: sans-serif;
+  }
+</style>
+<h1>Numverify Phone Number Verification</h1>
+<a href="/project4/numverify/phoneNumber.php">Click here to go to phone number verification page.</a>
