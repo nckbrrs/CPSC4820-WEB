567f001c2ccf621928eb42d580cded6e916898b6
Nick Barrs <nickbarrs@218.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-10-03 13:42:49 -0400

    working on p4

--- project4/numverify/verify.php
+++ project4/numverify/verify.php
@@ -7,12 +7,13 @@ session_start();
 if (!isset($_SESSION['username'])) {
   header("Location: /project4/login/index.html", true, 302);
 }
+/*
 // otherwise, redirect to phoneNumber.php
 // since there is no reason for a user to need to access this page
 else {
   header("Location: /project4/numverify/phoneNumber.php", true, 302);
 }
-
+*/
 // numverify variables
 $numverifyEndpoint = "http://apilayer.net/api/validate";
 $numverifyKey = "f7280f3f9e7e4bef446722c1923f2179";
