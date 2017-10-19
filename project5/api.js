dde381d54122b2d3f2aed208b747049926920c4c
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-10-19 01:10:00 -0400

    working on p5

--- project5/api.js
+++ project5/api.js
@@ -12,7 +12,7 @@ bluebird.promisifyAll(redis.Multi.prototype);
 // Express setup
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 
 // Redis setup
 var client = redis.createClient();
