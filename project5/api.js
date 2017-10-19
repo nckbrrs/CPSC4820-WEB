054aa42c186f7e03a8279ba7a1152d79c9750b79
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-10-18 22:47:37 -0400

    working on p5

--- project5/api.js
+++ project5/api.js
@@ -1,19 +1,22 @@
 var express = require('express');
-/*var redis = require('redis');
-var bluebird = require('bluebird');
+var redis = require('redis');
+/*var bluebird = require('bluebird');
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 */
 
 var app = express();
-//var dbClient = redis.createClient();
+var client = redis.createClient();
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
-  console.log('Got a request to \'/\'');
 });
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 })
+
+client.on('connection', function() {
+  console.log('connected!');
+});
