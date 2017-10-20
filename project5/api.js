d30c60771ee791ff704ce354ca01a13ee28f82d5
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-10-20 11:34:28 -0400

    working on p5

--- project5/api.js
+++ project5/api.js
@@ -31,38 +31,60 @@ var authenticate = function(req)  {
 
 // Actual API stuff
 app.get('/', function(req, res) {
-  console.log('basic get');
+  console.log('received get / request');
   res.send('Hello World!');
 });
 
 app.post('/students', function(req, res) {
-  console.log('post /students');
+  if (!authenticate(req)) {
+    res.status(401);
+    res.end();
+    return;
+  }
+
+  console.log('received post /students request');
   var studentObj = req.body;
+
+  // check for bad request (no body, no username field, or no name field)
   if (studentObj == null || studentObj['username'] == null || studentObj['name'] == null) {
-    console.log('bad request');
-    console.log('studentObj is ' + JSON.stringify(studentObj));
-    console.log('studentObj username is ' + studentObj.username);
-    console.log('studentObj name is ' + studentObj['name']);
-    res.status(400).send();
-  } else {
+    console.log('studentObj is null or no username or name');
+    console.log('studentObj is:');
+    console.log(JSON.stringify(studentObj));
+    res.status(400);
+    res.end();
+    return;
+  }
+  else {
+    // check if username already exists in redist "students" set
     client.sismemberAsync('students', `${studentObj['username']}`).then(function(exists) {
       if (!exists) {
         console.log('creating and adding new student');
+        // add _ref field to student object, with value of '/students/USERNAME'
         studentObj['_ref'] = `/students/${studentObj['username']}`;
+        // multi() used to execute several redis commands atomically
         client.multi()
+          // create a new hashmap for the newly created student called 'student:USERNAME',
+          // with the fields and keys located in the studentObj object
           .hmset(`student:${studentObj['username']}`, studentObj)
+          // add USERNAME to the 'students' set
           .sadd('students', `${studentObj['username']}`)
+          // execute the above redis commands atomically
           .execAsync().then(function(done) {
             console.log('successful execAsync');
-            res.status(200).json(studentObj);
+            // send body containing a reference to newly created student
+            res.status(200).json({_ref: '`${studentObj['_ref']}`');
+            return;
           },
           function(err) {
             console.log('error in execAsync');
             res.status(500).send(err);
+            return;
           });
       } else {
-        console.log('student exists');
-        res.status(400).send();
+        console.log('student already exists');
+        res.status(400);
+        res.end();
+        return;
       }
     });
   }
