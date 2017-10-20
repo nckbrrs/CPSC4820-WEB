cfa7fd20d3bdd42438571b5eb3680275a040073e
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-10-20 13:00:12 -0400

    working on p5

--- project5/api.js
+++ project5/api.js
@@ -20,7 +20,7 @@ client.on('error', function(err) {
   console.log('Error in redis client.on ' + err);
 });
 
-// Authentication
+// Authentication method
 var authenticate = function(req)  {
   var credentials = auth(req);
   if (!credentials || credentials.name !== 'teacher' || credentials.pass !== 't1g3rTester!@#') {
@@ -29,15 +29,17 @@ var authenticate = function(req)  {
   return true;
 }
 
-// Actual API stuff
+// Basic GET request for testing purposes
 app.get('/', function(req, res) {
   console.log('received get / request');
   res.send('Hello World!');
 });
 
+// Actual API stuff
 app.post('/students', function(req, res) {
   if (!authenticate(req)) {
-    res.status(401).send("not authenticated");
+    res.status(401);
+    res.end();
     return;
   }
 
@@ -46,8 +48,8 @@ app.post('/students', function(req, res) {
 
   // check for bad request (no body, no username field, or no name field)
   if (studentObj == null || studentObj['username'] == null || studentObj['name'] == null) {
-    console.log('studentObj is null or no username or name');
-    console.log('studentObj is:');
+    console.log('--studentObj is null or no username or name');
+    console.log('--studentObj is:');
     console.log(JSON.stringify(studentObj));
     res.status(400);
     res.end();
@@ -56,8 +58,8 @@ app.post('/students', function(req, res) {
   else {
     // check if username already exists in redist "students" set
     client.sismemberAsync('students', `${studentObj['username']}`).then(function(exists) {
-      if (!exists) {
-        console.log('creating and adding new student');
+      //if (!exists) {
+        console.log('--creating and adding new student');
         // add _ref field to student object, with value of '/students/USERNAME'
         studentObj['_ref'] = `/students/${studentObj['username']}`;
         // multi() used to execute several redis commands atomically
@@ -69,23 +71,46 @@ app.post('/students', function(req, res) {
           .sadd('students', `${studentObj['username']}`)
           // execute the above redis commands atomically
           .execAsync().then(function(done) {
-            console.log('successful execAsync');
+            console.log('--successful execAsync');
             // send body containing a reference to newly created student
             res.status(200).json({_ref: `${studentObj['_ref']}`});
             return;
           },
           function(err) {
-            console.log('error in execAsync');
+            console.log('--error in execAsync');
+            console.log(err);
             res.status(500).send(err);
             return;
-          });
+          });/*
       } else {
-        console.log('student already exists');
+        console.log('--student already exists');
         res.status(400);
         res.end();
-        return;
+        return;*/
       }
     });
+  }
+});
+
+app.delete('/students/:username', function(req, res) {
+  if (!authenticate(req)) {
+    res.status(401);
+    res.end();
+    return;
+  }
+
+  username = req.params.username;
+  client.sismemberAsync('students', username).then(function(exists) {
+    if (exists) {
+      client.sremAsync('students', username).then(function(res) {
+
+      })
+    } else {
+      console.log('--student does not exist');
+      res.status(404);
+      res.end();
+      return;
+    }
   }
 });
 
