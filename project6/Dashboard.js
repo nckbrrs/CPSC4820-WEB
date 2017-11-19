18305fd233fad705a0f8b4a1584a488107b7b635
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-19 17:58:21 -0500

    P6 WIP

--- project6/Dashboard.js
+++ project6/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardHeader, CardText } from 'matetrial-ui/Card';
+import {Card, CardHeader, CardText } from 'material-ui/Card';
 
 export default () => (
   <Card style={{ margin: '2em' }}>
