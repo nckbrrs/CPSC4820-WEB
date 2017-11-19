dd8e95883b293f5b6091387249357d66438aa3de
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-19 17:53:43 -0500

    P6 WIP

--- project6/App.js
+++ project6/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {jsonServerRestClient, Admin, Resource, Delete} from 'admin-on-rest';
 import {PostList, PostEdit, PostCreate} from './posts';
 import {UserList} from './users';
+import Dashboard from './Dashboard';
 
 const App = () => (
-  <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+  <Admin dashboard={Dashboard}restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete}/>
     <Resource name="users" list={UserList} />
   </Admin>
