854fad9cbcca637ed21a58f64b2af226482617c8
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-20 00:54:02 -0500

    P6 WIP

--- project6/api.js
+++ project6/api.js
@@ -14,9 +14,14 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  /*
   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");*/
   next();
 });
 
