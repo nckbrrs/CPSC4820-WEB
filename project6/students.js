ae29ba0a1f8356dfa5a3cfde9a105119d22cd0e1
Nick Barrs <nickbarrs@174.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-11-20 12:46:00 -0500

    P6 WIP

--- project6/students.js
+++ project6/students.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Responsive, List, SimpleList, Edit, Create, Datagrid, TextField, EditButton, DeleteButton, DisabledInput, SimpleForm, TextInput} from 'admin-on-rest';
 
 export const StudentList = (props) => (
-  <List {...props}>
+  <List {...props} filters={<GradeFilter />}>
     <Datagrid>
       <TextField source="id" />
       <TextField source="name" />
@@ -30,15 +30,9 @@ export const StudentCreate = (props) => (
   </Create>
 );
 
-/* FILTER ID AND NAME
-const GradeFilter = (props) => (
+const StudentFilter = (props) => (
   <Filter {...props}>
-    <ReferenceInput label="id" source="id" reference="grades" allowEmpty>
-      <SelectInput optionText="id" />
-    </ReferenceInput>
-    <ReferenceInput label="type" source="type" reference="grades" allowEmpty>
-      <SelectInput optionText="type" />
-    </ReferenceInput>
+    <TextInput source="id" />
+    <TextInput source="name" />
   </Filter>
 );
-*/
