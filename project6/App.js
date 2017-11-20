6b4631696731a38805ce4c18556a22166820ae9a
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-19 23:33:31 -0500

    P6 WIP

--- project6/App.js
+++ project6/App.js
@@ -7,7 +7,7 @@ import {GradeList, GradeEdit, GradeCreate} from './grades';
 
 const httpClient = (url, options = {}) => {
   if (!options.headers) {
-    options.headers = new Headers({ Accept: 'application/json',  });})
+    options.headers = new Headers({ Accept: 'application/json' });
   }/*
   const token = localStorage.getItem('token');
   options.headers.set('Authorization', `Basic ${token}`);*/
