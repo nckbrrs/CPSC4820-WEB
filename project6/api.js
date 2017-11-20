b55f9b9b6b042aad1dd560b8ee5c51241e200696
Nick Barrs <nickbarrs@174.182.21.198.tigernet.wifi.dyn.clemson.edu>
2017-11-20 13:04:39 -0500

    P6 WIP

--- project6/api.js
+++ project6/api.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET, DELETE, POST, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 });
 
