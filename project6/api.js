f6bd61a90a13dda7fc615602c56c3485eba35b8e
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-19 19:11:31 -0500

    P6 WIP

--- project6/api.js
+++ project6/api.js
@@ -141,7 +141,7 @@ app.put('/students/:id', function(req, res) {
   }
 
   // set fields of new student object to equal those in the request, and leave the rest alone
-  newStudentObj = client.hgetall(`student:${id}`);
+  newStudentObj = json(client.hgetall(`student:${id}`));
   newStudentObj['name'] = req.body['name'];
 
   // ensure that requested id already exists
