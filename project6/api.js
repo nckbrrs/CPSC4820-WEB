7813576f2b12f33ebe546e97bab9e8ee88fc72aa
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-19 19:53:49 -0500

    P6 WIP

--- project6/api.js
+++ project6/api.js
@@ -132,7 +132,7 @@ app.put('/students/:id', function(req, res) {
   }
 
   var id = req.params.id;
-  var oldStudentObj = {}, newStudentObj = {};
+  var newStudentObj = {};
 
   // check for bad request (no body, or no name field, or has id field)
   if (req.body['name'] == null || req.body['id'] != null) {
@@ -140,26 +140,18 @@ app.put('/students/:id', function(req, res) {
     return;
   }
 
-  client.hgetallAsync(`student:${id}`).then(function(studentObj) {
-    oldStudentObj = JSON.stringify(studentObj);
-    return;
-  });
-
   // set fields of new student object to equal those in the request, and leave the rest alone
-  newStudentObj['id'] = oldStudentObj['id'];
   newStudentObj['name'] = req.body['name'];
-  newStudentObj['_ref'] = oldStudentObj['_ref'];
 
   // ensure that requested id already exists
   client.sismemberAsync('students', id).then(function(exists) {
     if (exists) {
       // make requested changes to student's values
       client.hmsetAsync(`student:${id}`, newStudentObj).then(function(retval) {
-        res.status(200).json(newStudentObj);
-        /*client.hgetallAsync(`student:${id}`).then(function(studentObj) {
+        client.hgetallAsync(`student:${id}`).then(function(studentObj) {
           res.status(200).json(studentObj);
           return;
-        });*/
+        });
       });
     } else {
       // student does not exist
@@ -331,22 +323,21 @@ app.put('/grades/:gradeid', function(req, res) {
   }
 
   // set fields of new grade object to equal those in the request
-  client.hgetallAsync(`grade:${id}`).then(function(gradeObj) {
-    newGradeObj = JSON.stringify(gradeObj);
-    for (var field in fields) {
-      if (req.body[fields[field]] != null) {
-        newGradeObj[fields[field]] = req.body[fields[field]];
-      }
+  for (var field in fields) {
+    if (req.body[fields[field]] != null) {
+      newGradeObj[fields[field]] = req.body[fields[field]];
     }
-  });
+  }
 
   // ensure that requested grade already exists
   client.sismemberAsync('grades', gradeId).then(function(exists) {
     if (exists) {
       // make requested changes to grade's values
       client.hmsetAsync(`grade:${gradeId}`, newGradeObj).then(function(retval) {
-        res.status(200).json(newGradeObj);
-        return;
+        client.hgetallAsync(`student:${id}`).then(function(studentObj) {
+          res.status(200).json(gradeObj);
+          return;
+        });
       });
     } else {
       // grade does not exist
