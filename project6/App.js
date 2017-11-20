c20773c1c41c36feaae20193b5dde927a84317e2
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-19 22:13:10 -0500

    P6 WIP

--- project6/App.js
+++ project6/App.js
@@ -8,16 +8,16 @@ import authClient from './authClient';
 const httpClient = (url, options = {}) => {
   if (!options.headers) {
     options.headers = new Headers({ Accept: 'application/json' });
-  }
+  }/*
   const token = localStorage.getItem('token');
-  options.headers.set('Authorization', `Basic ${token}`);
+  options.headers.set('Authorization', `Basic ${token}`);*/
   return fetchUtils.fetchJson(url, options);
 }
 
 const restClient = jsonServerRestClient('http://54.200.82.249:3001', httpClient);
 
 const App = () => (
-  <Admin authClient={authClient} restClient={restClient}>
+  <Admin /*authClient={authClient}*/ restClient={restClient}>
     <Resource name="students" list={StudentList} edit={StudentEdit} create={StudentCreate} remove={Delete}/>
     <Resource name="grades" list={GradeList} edit={GradeEdit} create={GradeCreate} remove={Delete}/>
   </Admin>
