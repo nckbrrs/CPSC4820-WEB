d9b535b5868ebd90dc29a194755fa26a2d003ad2
Nick Barrs <nickbarrs@Nicks-MacBook-Pro.local>
2017-11-20 00:40:34 -0500

    P6 WIP

--- project6/api.js
+++ project6/api.js
@@ -15,7 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, POST, GET, OPTIONS, DELETE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
   next();
 });
 
